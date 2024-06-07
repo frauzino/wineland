ddac5abe74f320e20d2ab3b9264877cf33f31ae1
Richard Young <richarddfyoung@gmail.com>
2024-06-06 22:32:06 -0400

    fixed retailers_helper retailers_for_region_to_hash to combine all retailers in each city under one header

--- app/helpers/retailers_helper.rb
+++ app/helpers/retailers_helper.rb
@@ -1,8 +1,11 @@
 module RetailersHelper
   def retailers_for_region_to_hash(region)
-    Retailer.where(region: region).map do |retailer|
-      { header: retailer.city,
-        content: "#{retailer.name} - #{retailer.address}"}
+    cities_for_region = Retailer.where(region: region).distinct.pluck(:city)
+
+    cities_for_region.map do |city|
+      retailers_for_city = Retailer.where(region: region, city: city)
+      { header: city,
+        content: retailers_for_city.map { |retailer| "#{retailer.name} - #{retailer.address}" }.join('<br>') }
     end
   end
 end

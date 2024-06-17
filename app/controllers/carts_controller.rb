84a3449933c822ead84552c69a72281ec35c9dc7
Richard Young <richarddfyoung@gmail.com>
2024-06-17 12:31:59 -0400

    added cart functionality, styled cart, updated quantity_selector to pass values to cart

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,5 +1,5 @@
 class CartsController < ApplicationController
-  before_action :set_cart, only: %i[ show edit update destroy add_to_cart ]
+  before_action :set_cart, only: %i[ show edit update destroy add_to_cart remove_from_cart update_cart ]
 
   # GET /carts or /carts.json
   def index
@@ -35,24 +35,14 @@ class CartsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /carts/1 or /carts/1.json
-  def update
-    respond_to do |format|
-      if @cart.update(cart_params)
-        format.html { redirect_to cart_url(@cart), notice: "Cart was successfully updated." }
-        format.json { render :show, status: :ok, location: @cart }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @cart.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
   def add_to_cart
     product = Product.find(params[:product_id])
-    @cart.products << product
-    @cart.total_price += product.price
-    # @cart = Cart.find(session[:cart_id])
+    quantity = params[:quantities][params[:product_id]].to_i
+
+    quantity.times do
+      @cart.products << product
+      @cart.total_price += product.price
+    end
 
     if @cart.save
       redirect_to products_path, notice: 'Product was successfully added to cart.'
@@ -73,12 +63,34 @@ class CartsController < ApplicationController
     end
   end
 
+  def update_cart
+    params[:quantities].each do |product_id, quantity|
+      product = Product.find(product_id)
+      current_quantity = @cart.products.where(id: product_id).count
+      difference = quantity.to_i - current_quantity
+
+      if difference.positive?
+        difference.times { @cart.products << product }
+      elsif difference.negative?
+        @cart.product_carts.where(product_id: product_id).limit(difference.abs).destroy_all
+      end
+    end
+    @cart.total_price = @cart.products.sum(:price)
+
+    if @cart.save
+      redirect_to cart_path(@cart), notice: 'Cart was successfully updated.'
+    else
+      redirect_to cart_path(@cart), notice: 'Cart was not updated.'
+    end
+  end
+
   # DELETE /carts/1 or /carts/1.json
   def destroy
     @cart.destroy
+    session[:cart_id] = nil
 
     respond_to do |format|
-      format.html { redirect_to carts_url, notice: "Cart was successfully destroyed." }
+      format.html { redirect_to cart_url, notice: 'Cart was successfully emptied.' }
       format.json { head :no_content }
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -9,10 +9,12 @@ Rails.application.routes.draw do
   resources :contact, only: %i[new create]
   resources :users, only: %i[show]
   resources :orders, except: %i[new edit update]
-  resource :cart, only: %i[show]
+  resource :cart, only: %i[show destroy]
 
-  post 'add_to_cart/:product_id', to: 'carts#add_to_cart', as: 'add_to_cart'
+  # post 'add_to_cart/:product_id/:quantity', to: 'carts#add_to_cart', as: 'add_to_cart'
+  post 'add_to_cart', to: 'carts#add_to_cart', as: 'add_to_cart'
   delete 'remove_from_cart/:product_id', to: 'carts#remove_from_cart', as: 'remove_from_cart'
+  patch 'update_cart', to: 'carts#update_cart', as: 'update_cart'
   get 'pages/home', as: 'home'
   get 'pages/about', as: 'about'
   get 'pages/privacy', as: 'privacy'
